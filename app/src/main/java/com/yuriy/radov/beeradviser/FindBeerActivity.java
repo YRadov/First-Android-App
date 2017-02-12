3ed1dda4d14e1e7e1c5a216067ce979175b506f3
YRadov <radov.yuriy@ukr.net>
2017-02-12 21:56:48 +0200

    add method onClickFindBeer

--- app/src/main/java/com/yuriy/radov/beeradviser/FindBeerActivity.java
+++ app/src/main/java/com/yuriy/radov/beeradviser/FindBeerActivity.java
@@ -2,12 +2,18 @@ package com.yuriy.radov.beeradviser;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class FindBeerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_finde_beer);
+    }
+
+    // Call when the user click the button
+    public void onClickFindBeer(View view) {
+
     }
 }
